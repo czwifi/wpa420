0b9df29938ff4c32a35344b794e942a51b230a7a
czwifi <czwifi@protonmail.com>
2021-07-09 15:30:09 +0200

    Moved account related settings to /account/; added changing passwords

--- wpa420/settings.py
+++ wpa420/settings.py
@@ -146,4 +146,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
-LOGIN_URL = '/login'
+LOGIN_URL = 'account/login'
