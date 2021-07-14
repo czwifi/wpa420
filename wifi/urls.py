e6fe165a1fc8579ac24f595747996b8a8936e08b
czwifi <czwifi@protonmail.com>
2021-07-14 14:25:10 +0200

    Added a test favicon

--- wifi/urls.py
+++ wifi/urls.py
@@ -10,9 +10,6 @@ urlpatterns = [
     path('upload_json', views.upload_form_json, name='upload_json'),
     path('api/mobapp/getCollection', views.wifi_list_json, name='api_getCollection'),
     path('api/mobapp/dbHash', views.api_dbhash, name='api_dbHash'),
-<<<<<<< HEAD
-    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('img/favicon.ico')))
-=======
     path('account/login', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('account/logout', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('account/change_password', auth_views.PasswordChangeView.as_view(template_name='change_password.html', success_url='login'), name='change_password'),
@@ -20,5 +17,4 @@ urlpatterns = [
     path('account/register', views.register, name='register'),
     path('account/settings', views.settings, name='settings'),
     path('my_imports/refresh_location', views.refresh_location, name='refresh_location'),
->>>>>>> master
 ]
