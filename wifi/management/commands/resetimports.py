1e2176d3cb1649b058be1d31be1318a1bdc55b60
czwifi <czwifi@protonmail.com>
2021-08-12 21:10:06 +0200

    Added resetimports command

---
+++ wifi/management/commands/resetimports.py
@@ -0,0 +1,14 @@
+from django.core.management.base import BaseCommand, CommandError
+from wifi.models import AccessPoint, WifiImport
+from datetime import datetime, timedelta
+
+class Command(BaseCommand):
+    help = 'Resets timestamps to align with import times'
+
+    def handle(self, *args, **options):
+        access_points = AccessPoint.objects.all()
+        for access_point in access_points:
+            access_point.wifi_import = None
+            access_point.save()
+            print(f"Updated {access_point}")
+        print("Done")
