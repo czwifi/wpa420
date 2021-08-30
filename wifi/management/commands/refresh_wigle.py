a322989dfcd8d2a989d1f938c59540dd8641c3ce
czwifi <87139531+czwifi@users.noreply.github.com>
2021-08-30 17:03:15 +0200

    Collecting additional data about networks from Wigle (#3)
    * Added geolocation fields for networks
    
    * Added refresh_wigle command
    
    * Collecting more data about networks
    
    * Displaying the amount of unprocessed networks

---
+++ wifi/management/commands/refresh_wigle.py
@@ -0,0 +1,60 @@
+from django.core.management.base import BaseCommand, CommandError
+from wifi.models import AccessPoint, WifiImport
+from datetime import datetime, timedelta
+from requests.auth import HTTPBasicAuth
+import requests
+import json
+
+class BadWigleApiData(Exception):
+    pass
+
+class TooManyQueriesToday(Exception):
+    pass
+
+class Command(BaseCommand):
+    help = 'Loads Wigle info for all networks'
+
+    def add_arguments(self, parser):
+        parser.add_argument('wigle_name')
+        parser.add_argument('wigle_key')
+
+    def refresh_ap(self, ap, wigle_name, wigle_key):
+        wigle_info = requests.get(f'https://api.wigle.net/api/v2/network/detail', params={'netid': ap.bssid.lower()}, auth=HTTPBasicAuth(wigle_name, wigle_key))
+        if wigle_info.status_code != 200:
+            raise BadWigleApiData
+        wigle_info = json.loads(wigle_info.text)
+        #print(wigle_info)
+        if wigle_info['success'] is False and wigle_info['message'] == 'too many queries today.':
+            raise TooManyQueriesToday
+        ap.location_refreshed = datetime.now()
+        ap.refresh_attempts += 1
+        if wigle_info['success'] is True:
+            ap_info = wigle_info['results'][0]
+
+            ap.latitude = ap_info['trilat']
+            ap.longitude = ap_info['trilong']
+            ap.channel = ap_info['channel']
+            ap.city = ap_info['city']
+            ap.country = ap_info['country']
+            ap.encryption = ap_info['encryption']
+            ap.wigle_firsttime = ap_info['firsttime']
+            ap.housenumber = ap_info['housenumber']
+            ap.wigle_lasttime = ap_info['lasttime']
+            ap.wigle_lastupdt = ap_info['lastupdt']
+            ap.name = ap_info['name']
+            ap.postalcode = ap_info['postalcode']
+            ap.region = ap_info['region']
+            ap.road = ap_info['road']
+            ap.wigle_ssid = ap_info['ssid']
+            ap.wigle_qos = ap_info['qos']
+            ap.wigle_type = ap_info['type']
+            if ap.frequency == None:
+                ap.frequency = AccessPoint.Frequency.FREQ_2_4G if ap.channel <= 14 else AccessPoint.Frequency.FREQ_5G
+        ap.save()
+        print(f"{ap.bssid}")
+        #print(wigle_info)
+
+    def handle(self, *args, **options):
+        ap_list = AccessPoint.objects.filter(wigle_ssid=None, refresh_attempts=0).order_by('location_refreshed')
+        for ap in ap_list:
+            self.refresh_ap(ap, options['wigle_name'], options['wigle_key'])
