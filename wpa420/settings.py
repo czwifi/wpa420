b53e0d07f4b01a4288e735d5fbadc87811812b84
czwifi <czwifi@protonmail.com>
2021-09-14 01:40:39 +0200

    Data Wifi List JSON caching

--- wpa420/settings.py
+++ wpa420/settings.py
@@ -82,6 +82,22 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'wpa420.wsgi.application'
 
+DEBUG_TOOLBAR_PANELS = [
+    'debug_toolbar.panels.history.HistoryPanel',
+    'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    'debug_toolbar.panels.templates.TemplatesPanel',
+    'debug_toolbar.panels.cache.CachePanel',
+    'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.logging.LoggingPanel',
+    'debug_toolbar.panels.redirects.RedirectsPanel',
+    'debug_toolbar.panels.profiling.ProfilingPanel',
+]
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
